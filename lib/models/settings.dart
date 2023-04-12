4a06a704fd8c1b81e9848cef78f1b6629ac17936
Archanciel <jp.schnyder@gmail.com>
2023-04-12 23:03:46 +0200

    works

--- lib/models/settings.dart
+++ lib/models/settings.dart
@@ -1,9 +1,15 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+import 'dart:io';
+
 import '../constants.dart';
 
 enum SettingType {
   theme,
   language,
   audio,
+  audioTwo,
 }
 
 enum Theme {
@@ -22,13 +28,23 @@ enum Audio {
   defaultQuality,
 }
 
+enum AudioTwo {
+  rootPath,
+  pathLst,
+  defaultQuality,
+}
+
 class Settings {
   final Map<SettingType, Map<dynamic, dynamic>> _settings = {
     SettingType.theme: {SettingType.theme: Theme.dark},
     SettingType.language: {SettingType.language: Language.english},
     SettingType.audio: {
       Audio.rootPath: kDownloadAppDir,
-      Audio.pathLst: ["", "books", "music"]
+      Audio.pathLst: ["empty", "books", "music"]
+    },
+    SettingType.audioTwo: {
+      AudioTwo.rootPath: kDownloadAppDir,
+      AudioTwo.pathLst: ["EMPTY", "BOOKS", "MUSIC"]
     },
   };
 
@@ -40,12 +56,54 @@ class Settings {
   }) {
     return _settings[settingType]![settingSubType];
   }
+
+  void set({
+    required SettingType settingType,
+    required dynamic settingSubType,
+    required dynamic value,
+  }) {
+    _settings[settingType]![settingSubType] = value;
+  }
 }
 
-void main(List<String> args) {
-  Settings settings = Settings();
+Future<void> main(List<String> args) async {
+  Settings initialSettings = Settings();
 
-  print('${settings.get(settingType: SettingType.theme, settingSubType: SettingType.theme)}');
-  print('${settings.get(settingType: SettingType.audio, settingSubType: Audio.rootPath)}');
-  print('${settings.get(settingType: SettingType.audio, settingSubType: Audio.pathLst)}');
+  print(
+      '${initialSettings.get(settingType: SettingType.theme, settingSubType: SettingType.theme)}');
+  print(
+      '${initialSettings.get(settingType: SettingType.audio, settingSubType: Audio.rootPath)}');
+  print(
+      '${initialSettings.get(settingType: SettingType.audio, settingSubType: Audio.pathLst)}');
+  print(
+      '${initialSettings.get(settingType: SettingType.audioTwo, settingSubType: AudioTwo.pathLst)}');
+
+  initialSettings.set(
+      settingType: SettingType.theme,
+      settingSubType: SettingType.theme,
+      value: Theme.clear);
+
+  initialSettings.set(
+      settingType: SettingType.audio,
+      settingSubType: Audio.rootPath,
+      value: kDownloadAppTestDirWindows);
+
+  initialSettings.set(
+      settingType: SettingType.audio,
+      settingSubType: Audio.pathLst,
+      value: ['one', 'two']);
+
+  initialSettings.set(
+      settingType: SettingType.audioTwo,
+      settingSubType: AudioTwo.pathLst,
+      value: ['ONE', 'TWO']);
+
+  print(
+      '${initialSettings.get(settingType: SettingType.theme, settingSubType: SettingType.theme)}');
+  print(
+      '${initialSettings.get(settingType: SettingType.audio, settingSubType: Audio.rootPath)}');
+  print(
+      '${initialSettings.get(settingType: SettingType.audio, settingSubType: Audio.pathLst)}');
+  print(
+      '${initialSettings.get(settingType: SettingType.audioTwo, settingSubType: AudioTwo.pathLst)}');
 }
