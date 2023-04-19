abe6e7132c5e0f23251679fa6903f2b4abc786c3
Archanciel <jp.schnyder@gmail.com>
2023-04-20 00:35:36 +0200

    copied from chatgpt and modified

---
+++ lib/views/expandable_playlist_list_view.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/list_model.dart';
+import '../viewmodels/expandable_playlist_list_vm.dart';
+
+class ExpandablePlaylistListView extends StatefulWidget {
+  @override
+  State<ExpandablePlaylistListView> createState() =>
+      _ExpandablePlaylistListViewState();
+}
+
+class _ExpandablePlaylistListViewState
+    extends State<ExpandablePlaylistListView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: ElevatedButton(
+                key: const Key('toggle_button'),
+                onPressed: () {
+                  Provider.of<ExpandablePlaylistListVM>(context, listen: false)
+                      .toggleList();
+                },
+                child: const Text('Toggle List'),
+              ),
+            ),
+            Expanded(
+              child: ElevatedButton(
+                key: const Key('delete_button'),
+                onPressed: Provider.of<ExpandablePlaylistListVM>(context)
+                        .isButton1Enabled
+                    ? () {
+                        Provider.of<ExpandablePlaylistListVM>(context,
+                                listen: false)
+                            .deleteSelectedItem(context);
+                      }
+                    : null,
+                child: const Text('Delete'),
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                key: const Key('move_up_button'),
+                onPressed: Provider.of<ExpandablePlaylistListVM>(context)
+                        .isButton2Enabled
+                    ? () {
+                        Provider.of<ExpandablePlaylistListVM>(context,
+                                listen: false)
+                            .moveSelectedItemUp();
+                      }
+                    : null,
+                padding: const EdgeInsets.all(0),
+                icon: const Icon(
+                  Icons.arrow_drop_up,
+                  size: 50,
+                ),
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                key: const Key('move_down_button'),
+                onPressed: Provider.of<ExpandablePlaylistListVM>(context)
+                        .isButton3Enabled
+                    ? () {
+                        Provider.of<ExpandablePlaylistListVM>(context,
+                                listen: false)
+                            .moveSelectedItemDown();
+                      }
+                    : null,
+                padding: const EdgeInsets.all(0),
+                icon: const Icon(
+                  Icons.arrow_drop_down,
+                  size: 50,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Consumer<ExpandablePlaylistListVM>(
+          builder: (context, listViewModel, child) {
+            if (listViewModel.isListExpanded) {
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: listViewModel.playlistList.length,
+                  itemBuilder: (context, index) {
+                    ListItem item = listViewModel.playlistList[index];
+                    return ListTile(
+                      title: Text(item.name),
+                      trailing: Checkbox(
+                        value: item.isSelected,
+                        onChanged: (value) {
+                          listViewModel.selectItem(context, index);
+                        },
+                      ),
+                    );
+                  },
+                ),
+              );
+            } else {
+              return Container();
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
