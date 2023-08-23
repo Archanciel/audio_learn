6676c64bd819176a854b280784922edf73c19b72
Archanciel <jp.schnyder@gmail.com>
2023-08-23 18:26:19 +0200

    getVideoDescriptionChapters code and test ok

--- lib/viewmodels/audio_download_vm.dart
+++ lib/viewmodels/audio_download_vm.dart
@@ -1048,4 +1048,100 @@ class AudioDownloadVM extends ChangeNotifier {
 
     notifyListeners();
   }
+
+  /// Returns a map containing the chapters names and their HH:mm:ss
+  /// time position in the audio.
+  Map<String, String> getVideoDescriptionChapters({
+    required String videoDescription,
+  }) {
+    // Extract the "TIME CODE" section from the description.
+    String timeCodeSection = videoDescription.split('TIME CODE :').last;
+
+    // Define a pattern to match time codes and chapter names.
+    RegExp pattern = RegExp(r'(\d{1,2}:\d{2}(?::\d{2})?)\s+(.+)');
+
+    // Use the pattern to find matches in the time code section.
+    Iterable<RegExpMatch> matches = pattern.allMatches(timeCodeSection);
+
+    // Create a map to hold the time codes and chapter names.
+    Map<String, String> chapters = <String, String>{};
+
+    for (var match in matches) {
+      var timeCode = match.group(1)!;
+      var chapterName = match.group(2)!;
+      chapters[chapterName] = timeCode;
+    }
+
+    return chapters;
+  }
+}
+
+void main() {
+  WarningMessageVM warningMessageVM = WarningMessageVM();
+  AudioDownloadVM audioDownloadVM = AudioDownloadVM(
+    warningMessageVM: warningMessageVM,
+  );
+
+  String videoDescription = '''Ma chaîne YouTube principale
+  https://www.youtube.com/@LeFuturologue
+
+
+  ME SOUTENIR FINANCIÈREMENT :
+
+  Sur Tipeee
+  https://fr.tipeee.com/le-futurologue
+  Sur PayPal
+  https://www.paypal.com/donate/?hosted_button_id=BBXFGSM5D5WQS
+  Sur Patreon
+  https://patreon.com/LeFuturologue
+
+
+  MES VIDÉOS COURTES :
+
+  Sur YouTube 
+  https://youtube.com/@LeFuturologue/shorts
+  Sur Instagram
+  https://www.instagram.com/le.futurologue/
+  Sur TikTok
+  https://www.tiktok.com/@le.futurologue
+
+
+  TIME CODE :
+
+  0:00 Introduction
+  1:37 Qui es-tu ?
+  3:29 Les IA vont-elles tous nous mettre au chômage ?
+  21:01 Faut-il mettre les IA en open source ? 
+  48:05 Comment fonctionne les agents ?
+  1:14:31 Définition de l’IA autonome, de l’IA générale et de la super IA
+  1:41:23 Que manque-t-il pour avoir une AGI ?
+  1:57:23 À quel point faut-il avoir peur de L’IA ?
+  2:04:36 Les meilleurs arguments de ceux qui ne croient pas aux risques existentiels des AGI 
+  2:11:48 Y aura-t-il plusieurs AGI ?
+  2:14:06 Est-ce que l’explosion d’intelligence sera rapide ? 
+  2:22:37 Quels impacts aurait une IA qui devient consciente ?
+  2:53:18 Quelle est la probabilité qu’on arrive à aligner une AGI avant qu’on en crée une ?
+  3:09:54 Ressources pour aller plus loin
+  3:11:57 Un message pour l’humanité 
+
+
+  RESSOURCES MENTIONNÉES :
+
+  La chaîne YouTube de Jérémy
+  https://youtube.com/@suboptimalchannel9704
+  Le Twitter de Jérémy
+  https://twitter.com/suboptimalc?s=21&t=KiEIZQwoZSOhseL0LUGLpg
+  L’organisation « EffiSciences »
+  https://www.effisciences.org/
+
+  ChatGPT''';
+
+  Map<String, String> chapters = audioDownloadVM.getVideoDescriptionChapters(
+    videoDescription: videoDescription,
+  );
+
+  // Print the chapters.
+  for (var chapter in chapters.entries) {
+    print('${chapter.key}: ${chapter.value}');
+  }
 }
---
+++ test/viewmodels/audio_download_vm_test.dart
@@ -0,0 +1,99 @@
+import 'package:audio_learn/viewmodels/audio_download_vm.dart';
+import 'package:audio_learn/viewmodels/warning_message_vm.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:audio_learn/models/audio.dart';
+import 'package:audio_learn/models/playlist.dart';
+
+void main() {
+  group('AudioDownloadVM video description', () {
+    test('Test extract video chapters time position', () {
+      WarningMessageVM warningMessageVM = WarningMessageVM();
+      AudioDownloadVM audioDownloadVM = AudioDownloadVM(
+        warningMessageVM: warningMessageVM,
+      );
+
+      String videoDescription = '''Ma chaîne YouTube principale
+  https://www.youtube.com/@LeFuturologue
+
+
+  ME SOUTENIR FINANCIÈREMENT :
+
+  Sur Tipeee
+  https://fr.tipeee.com/le-futurologue
+  Sur PayPal
+  https://www.paypal.com/donate/?hosted_button_id=BBXFGSM5D5WQS
+  Sur Patreon
+  https://patreon.com/LeFuturologue
+
+
+  MES VIDÉOS COURTES :
+
+  Sur YouTube 
+  https://youtube.com/@LeFuturologue/shorts
+  Sur Instagram
+  https://www.instagram.com/le.futurologue/
+  Sur TikTok
+  https://www.tiktok.com/@le.futurologue
+
+
+  TIME CODE :
+
+  0:00 Introduction
+  1:37 Qui es-tu ?
+  3:29 Les IA vont-elles tous nous mettre au chômage ?
+  21:01 Faut-il mettre les IA en open source ? 
+  48:05 Comment fonctionne les agents ?
+  1:14:31 Définition de l’IA autonome, de l’IA générale et de la super IA
+  1:41:23 Que manque-t-il pour avoir une AGI ?
+  1:57:23 À quel point faut-il avoir peur de L’IA ?
+  2:04:36 Les meilleurs arguments de ceux qui ne croient pas aux risques existentiels des AGI 
+  2:11:48 Y aura-t-il plusieurs AGI ?
+  2:14:06 Est-ce que l’explosion d’intelligence sera rapide ? 
+  2:22:37 Quels impacts aurait une IA qui devient consciente ?
+  2:53:18 Quelle est la probabilité qu’on arrive à aligner une AGI avant qu’on en crée une ?
+  3:09:54 Ressources pour aller plus loin
+  3:11:57 Un message pour l’humanité 
+
+
+  RESSOURCES MENTIONNÉES :
+
+  La chaîne YouTube de Jérémy
+  https://youtube.com/@suboptimalchannel9704
+  Le Twitter de Jérémy
+  https://twitter.com/suboptimalc?s=21&t=KiEIZQwoZSOhseL0LUGLpg
+  L’organisation « EffiSciences »
+  https://www.effisciences.org/
+
+  ChatGPT''';
+
+      Map<String, String> expectedChapters = {
+        'Introduction': '0:00',
+        'Qui es-tu ?': '1:37',
+        'Les IA vont-elles tous nous mettre au chômage ?': '3:29',
+        'Faut-il mettre les IA en open source ? ': '21:01',
+        'Comment fonctionne les agents ?': '48:05',
+        'Définition de l’IA autonome, de l’IA générale et de la super IA':
+            '1:14:31',
+        'Que manque-t-il pour avoir une AGI ?': '1:41:23',
+        'À quel point faut-il avoir peur de L’IA ?': '1:57:23',
+        'Les meilleurs arguments de ceux qui ne croient pas aux risques existentiels des AGI ':
+            '2:04:36',
+        'Y aura-t-il plusieurs AGI ?': '2:11:48',
+        'Est-ce que l’explosion d’intelligence sera rapide ? ': '2:14:06',
+        'Quels impacts aurait une IA qui devient consciente ?': '2:22:37',
+        'Quelle est la probabilité qu’on arrive à aligner une AGI avant qu’on en crée une ?':
+            '2:53:18',
+        'Ressources pour aller plus loin': '3:09:54',
+        'Un message pour l’humanité ': '3:11:57',
+      };
+
+      Map<String, String> chapters =
+          audioDownloadVM.getVideoDescriptionChapters(
+        videoDescription: videoDescription,
+      );
+
+      expect(chapters, expectedChapters);
+    });
+  });
+}
