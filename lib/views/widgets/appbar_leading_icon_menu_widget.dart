7f5137d2bb8bcb758c021245ed0d272482edc736
Archanciel <jp.schnyder@gmail.com>
2023-10-17 00:05:47 +0200

    dart fix

--- lib/views/widgets/appbar_leading_icon_menu_widget.dart
+++ lib/views/widgets/appbar_leading_icon_menu_widget.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import '../../../viewmodels/language_provider.dart';
 import '../../../viewmodels/theme_provider.dart';
 
 enum AppBarPopupMenu { option1, option2 }
