52e04defa97d543e3f9c1c3cc7409d07dc8922ea
Archanciel <jp.schnyder@gmail.com>
2023-11-20 00:40:44 +0100

    now test works. Bingo !

---
+++ integration_test/async_audio_player_view_integration_test.dart
@@ -0,0 +1,156 @@
+import 'dart:async';
+
+import 'package:fake_async/fake_async.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:path/path.dart' as path;
+import 'package:audio_learn/constants.dart';
+import 'package:audio_learn/services/settings_data_service.dart';
+import 'package:audio_learn/utils/dir_util.dart';
+import 'package:audio_learn/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('Async AudioPlayerView Integration Tests', () {
+    testWidgets(
+        'Opening AudioPlayerView by clicking on audio title. Then async play and pause audio',
+        (
+      WidgetTester tester,
+    ) async {
+      const String audioPlayerSelectedPlaylistTitle =
+          'audio_player_view_2_shorts_test';
+      const String lastDownloadedAudioTitle = 'morning _ cinematic video';
+
+      await initializeApplication(
+        tester: tester,
+        savedTestDataDirName: 'audio_player_view_test',
+        selectedPlaylistTitle: audioPlayerSelectedPlaylistTitle,
+      );
+
+      // Now we want to tap on the lastly downloaded audio of the
+      // playlist in order to open the AudioPlayerView displaying
+      // the currently paused audio
+
+      // First, get the lastly downloaded Audio ListTile Text
+      // widget finder and tap on it
+      final Finder lastAudioListTileTextWidgetFinder =
+          find.text(lastDownloadedAudioTitle);
+
+      await tester.tap(lastAudioListTileTextWidgetFinder);
+      await tester.pumpAndSettle();
+
+      await tester
+          .tap(find.byIcon(Icons.play_arrow)); // Replace with your interaction
+      await tester.pumpAndSettle();
+
+      await Future.delayed(const Duration(seconds: 1));
+      await tester.pumpAndSettle();
+
+      // Verify if the play button changes to pause button
+      expect(find.byIcon(Icons.pause), findsOneWidget);
+
+      // Check if the widget's state reflects that audio has been playing for 10 seconds
+      // expect(find.text('10 seconds played'), findsOneWidget); // Replace with your assertion
+
+      // Additional checks or interactions can be performed here
+      // });
+
+      // Test play button
+      // final playButton = find.byIcon(Icons.play_arrow);
+      // await tester.tap(playButton);
+
+      // await tester.pumpAndSettle();
+
+      // Verify if the play button changes to pause button
+      // expect(find.byIcon(Icons.pause), findsOneWidget);
+
+      // Test pause button
+      // final pauseButton = find.byIcon(Icons.pause);
+      // await tester.tap(pauseButton);
+      // await tester.pumpAndSettle();
+
+      // // Verify if the pause button changes back to play button
+      // expect(find.byIcon(Icons.play_arrow), findsOneWidget);
+
+      // Add more tests as needed for slider movement, next/previous buttons, etc.
+
+      // Purge the test playlist directory so that the created test
+      // files are not uploaded to GitHub
+      DirUtil.deleteFilesInDirAndSubDirs(rootPath: kDownloadAppTestDirWindows);
+    });
+  });
+}
+
+Future<void> initializeApplication({
+  required WidgetTester tester,
+  required String savedTestDataDirName,
+  required String selectedPlaylistTitle,
+}) async {
+  // Purge the test playlist directory if it exists so that the
+  // playlist list is empty
+  DirUtil.deleteFilesInDirAndSubDirs(
+    rootPath: kDownloadAppTestDirWindows,
+    deleteSubDirectoriesAsWell: true,
+  );
+
+  // Copy the test initial audio data to the app dir
+  DirUtil.copyFilesFromDirAndSubDirsToDirectory(
+    sourceRootPath:
+        "$kDownloadAppTestSavedDataDir${path.separator}$savedTestDataDirName",
+    destinationRootPath: kDownloadAppTestDirWindows,
+  );
+
+  SettingsDataService settingsDataService = SettingsDataService(isTest: true);
+
+  // Load the settings from the json file. This is necessary
+  // otherwise the ordered playlist titles will remain empty
+  // and the playlist list will not be filled with the
+  // playlists available in the download app test dir
+  settingsDataService.loadSettingsFromFile(
+      jsonPathFileName:
+          "$kDownloadAppTestDirWindows${path.separator}$kSettingsFileName");
+
+  await app.main(['test']);
+  await tester.pumpAndSettle();
+
+  // Tap the 'Toggle List' button to show the list. If the list
+  // is not opened, checking that a ListTile with the title of
+  // the playlist was added to the list will fail
+  await tester.tap(find.byKey(const Key('playlist_toggle_button')));
+  await tester.pumpAndSettle();
+
+  // Find the ListTile Playlist containing the audio to copy to
+  // the target local playlist
+
+  // First, find the Playlist ListTile Text widget
+  final Finder audioPlayerSelectedPlaylistFinder =
+      find.text(selectedPlaylistTitle);
+
+  // Then obtain the Playlist ListTile widget enclosing the Text widget
+  // by finding its ancestor
+  final Finder selectedPlaylistListTileWidgetFinder = find.ancestor(
+    of: audioPlayerSelectedPlaylistFinder,
+    matching: find.byType(ListTile),
+  );
+
+  // Now find the Checkbox widget located in the Playlist ListTile
+  // and tap on it to select the playlist
+  final Finder selectedPlaylistCheckboxWidgetFinder = find.descendant(
+    of: selectedPlaylistListTileWidgetFinder,
+    matching: find.byType(Checkbox),
+  );
+
+  // Retrieve the Checkbox widget
+  final Checkbox checkbox =
+      tester.widget<Checkbox>(selectedPlaylistCheckboxWidgetFinder);
+
+  // Check if the checkbox is checked
+  if (checkbox.value == null || !checkbox.value!) {
+    // Tap the ListTile Playlist checkbox to select it
+    // so that the playlist audios are listed
+    await tester.tap(selectedPlaylistCheckboxWidgetFinder);
+    await tester.pumpAndSettle();
+  }
+}
