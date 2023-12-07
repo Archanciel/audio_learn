99bb4124403fa3c2859a2dd6d3f6fd406b2d5079
Archanciel <jp.schnyder@gmail.com>
2023-12-07 13:14:17 +0100

    improved documentation

--- test/viewmodels/mock_explore_test.dart
+++ test/viewmodels/mock_explore_test.dart
@@ -13,6 +13,9 @@ late MockYoutubeExplode mockYoutubeExplode;
 late MockYoutubeVideo mockYoutubeVideo;
 late MockVideoClient mockVideoClient;
 
+// Executing dart run build_runner build generates the mock
+// classes in the test/viewmodels/mock_explore_test.mocks.dart
+// file
 @GenerateMocks([YoutubeExplode, YoutubeVideo, yt.VideoClient])
 void main() {
   setUp(() {
@@ -20,12 +23,22 @@ void main() {
     mockYoutubeVideo = MockYoutubeVideo();
     mockVideoClient = MockVideoClient();
 
-    // Stubbing the 'videos' property
+    // Stubbing the 'videos' property.
+    //
+    // This is necessary because the 'videos' property is
+    // a getter and cannot be stubbed directly:
+    // mockYoutubeExplode.videos.get(any) will not work.
+    // Instead, we stub the getter to return a mock of the
+    // VideoClient class, which has a 'get' method that
+    // can be stubbed.
     when(mockYoutubeExplode.videos).thenReturn(mockVideoClient);
   });
 
   test('Test getVideo functionality', () async {
-    // Assuming 'get' is a method in the mockVideoClient
+    // Assuming 'get' is a method in the mockVideoClient.
+    // Possible because we stubbed the 'videos' property in
+    // the setUp method above with
+    // when(mockYoutubeExplode.videos).thenReturn(mockVideoClient);
     when(mockVideoClient.get(any)).thenAnswer((_) async => mockYoutubeVideo);
 
     // Your test code here
