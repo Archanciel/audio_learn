84d580cd7a9da30715bcc53a46bdb96b24768998
Archanciel <jp.schnyder@gmail.com>
2023-12-10 11:57:00 +0100

    improved yt.Video constructor. test works.

--- test/viewmodels/custom_mock_video_client.dart
+++ test/viewmodels/custom_mock_video_client.dart
@@ -1,21 +1,24 @@
+import 'package:audio_learn/constants.dart';
+
 import 'mock_youtube.mocks.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart' as yt;
 
 class CustomMockVideoClient extends MockVideoClient {
   @override
   Future<yt.Video> get(dynamic id) async {
-    List<String> keywords = ['test', 'video', 'audio', 'download'];
+    List<String> keywords = ['keyword 1', 'keyword 2', 'keyword 3'];
+
     return yt.Video(
       yt.VideoId(id.toString()), // Assuming 'id' is a valid video ID
       'CustomMockVideoClient Video Title',
       'CustomMockVideoClient Author',
       yt.ChannelId('CustomMockVideoClient_ChannelId'),
       DateTime.now(),
-      DateTime.now().toString(),
+      frenchDateTimeFormat.format(DateTime.now()),
       DateTime.now(),
       'CustomMockVideoClient video description',
       const Duration(minutes: 50),
-      const yt.ThumbnailSet(''), // Thumbnails
+      yt.ThumbnailSet(id.toString()), // Thumbnails
       keywords,
       const yt.Engagement(1, 10, 2),
       false,
