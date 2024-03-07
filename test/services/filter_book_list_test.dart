ad9534beb5af6d4fb3ba6b14d5c7a0382e1952b7
Archanciel <jp.schnyder@gmail.com>
2024-03-07 08:07:27 +0100

    tests ok

--- test/services/filter_book_list_test.dart
+++ test/services/filter_book_list_test.dart
@@ -29,7 +29,7 @@ void main() {
   Book bookOne = Book('Sur quelle tendance crypto investir en 2024 ?', '');
   Book bookTwo = Book('Tendance crypto en progression en 2024', '');
   Book bookThree = Book(
-      'Intelligence artificielle: quelle menace ou opportunité en 2024 ?', '');
+      'Intelligence Artificielle: quelle menace ou opportunité en 2024 ?', '');
   Book bookFour =
       Book('Intelligence humaine ou artificielle, quelles différences ?', '');
   List<Book> books = [
@@ -39,7 +39,7 @@ void main() {
     bookFour,
   ];
 
-  group('filter book list on title test', () {
+  group('ignoring case, filter book list on title test', () {
     test('filter by <tendance crypto> AND <en 2024>', () {
       List<Book> expectedFilteredBooks = [
         bookOne,
@@ -47,12 +47,13 @@ void main() {
       ];
 
       List<Book> filteredBooks = filter(
-          books,
-          [
+          booksLst: books,
+          filterSentences: [
             'tendance crypto',
             'en 2024',
           ],
-          FilterType.AND);
+          filterType: FilterType.AND,
+          ignoreCase: true);
 
       expect(filteredBooks, expectedFilteredBooks);
     });
@@ -64,12 +65,13 @@ void main() {
       ];
 
       List<Book> filteredBooks = filter(
-          books,
-          [
+          booksLst: books,
+          filterSentences: [
             'tendance crypto',
             'en 2024',
           ],
-          FilterType.OR);
+          filterType: FilterType.OR,
+          ignoreCase: true);
 
       expect(filteredBooks, expectedFilteredBooks);
     });
@@ -80,12 +82,13 @@ void main() {
       ];
 
       List<Book> filteredBooks = filter(
-          books,
-          [
+          booksLst: books,
+          filterSentences: [
             'en 2024',
             'tendance crypto',
           ],
-          FilterType.AND);
+          filterType: FilterType.AND,
+          ignoreCase: true);
 
       expect(filteredBooks, expectedFilteredBooks);
     });
@@ -97,12 +100,13 @@ void main() {
       ];
 
       List<Book> filteredBooks = filter(
-          books,
-          [
+          booksLst: books,
+          filterSentences: [
             'en 2024',
             'tendance crypto',
           ],
-          FilterType.OR);
+          filterType: FilterType.OR,
+          ignoreCase: true);
 
       expect(filteredBooks, expectedFilteredBooks);
     });
@@ -113,12 +117,13 @@ void main() {
       ];
 
       List<Book> filteredBooks = filter(
-          books,
-          [
+          booksLst: books,
+          filterSentences: [
             'quelle',
             '2024',
           ],
-          FilterType.AND);
+          filterType: FilterType.AND,
+          ignoreCase: true);
 
       expect(filteredBooks, expectedFilteredBooks);
     });
@@ -131,12 +136,13 @@ void main() {
       ];
 
       List<Book> filteredBooks = filter(
-          books,
-          [
+          booksLst: books,
+          filterSentences: [
             'quelle',
             '2024',
           ],
-          FilterType.OR);
+          filterType: FilterType.OR,
+          ignoreCase: true);
 
       expect(filteredBooks, expectedFilteredBooks);
     });
@@ -147,12 +153,13 @@ void main() {
       ];
 
       List<Book> filteredBooks = filter(
-          books,
-          [
+          booksLst: books,
+          filterSentences: [
             '2024',
             'quelle',
           ],
-          FilterType.AND);
+          filterType: FilterType.AND,
+          ignoreCase: true);
 
       expect(filteredBooks, expectedFilteredBooks);
     });
@@ -165,82 +172,207 @@ void main() {
       ];
 
       List<Book> filteredBooks = filter(
-          books,
-          [
+          booksLst: books,
+          filterSentences: [
             '2024',
             'quelle',
           ],
-          FilterType.OR);
+          filterType: FilterType.OR,
+          ignoreCase: true);
+
+      expect(filteredBooks, expectedFilteredBooks);
+    });
+      test('filter by <intelligence> OR <artificielle>', () {
+      List<Book> expectedFilteredBooks = [
+        bookThree,
+        bookFour,
+      ];
+
+      List<Book> filteredBooks = filter(
+          booksLst: books,
+          filterSentences: [
+            'intelligence',
+            'artificielle',
+          ],
+          filterType: FilterType.OR,
+          ignoreCase: true);
 
       expect(filteredBooks, expectedFilteredBooks);
     });
   });
+  group('not ignoring case, filter book list on title test', () {
+    test('filter by <tendance crypto> AND <en 2024>', () {
+      List<Book> expectedFilteredBooks = [
+        bookOne,
+      ];
 
-  List<String> filterSentences = [
-    'tendance crypto',
-    'en 2024',
-  ];
+      List<Book> filteredBooks = filter(
+          booksLst: books,
+          filterSentences: [
+            'tendance crypto',
+            'en 2024',
+          ],
+          filterType: FilterType.AND,
+          ignoreCase: false);
 
-  print(
-      '\nbooks list after AND filter sentences <tendance crypto> and <en 2024>');
-  printBooks(filter(books, filterSentences, FilterType.AND));
+      expect(filteredBooks, expectedFilteredBooks);
+    });
+    test('filter by <tendance crypto> OR <en 2024>', () {
+      List<Book> expectedFilteredBooks = [
+        bookOne,
+        bookTwo,
+        bookThree,
+      ];
 
-  print(
-      '\nbooks list after OR filter sentences <tendance crypto> and <en 2024>');
-  printBooks(filter(books, filterSentences, FilterType.OR));
+      List<Book> filteredBooks = filter(
+          booksLst: books,
+          filterSentences: [
+            'tendance crypto',
+            'en 2024',
+          ],
+          filterType: FilterType.OR,
+          ignoreCase: false);
 
-  filterSentences = [
-    'en 2024',
-    'tendance crypto',
-  ];
+      expect(filteredBooks, expectedFilteredBooks);
+    });
+    test('filter by <en 2024> AND <tendance crypto>', () {
+      List<Book> expectedFilteredBooks = [
+        bookOne,
+      ];
 
-  print(
-      '\nbooks list after AND filter sentences <en 2024> and <tendance crypto>');
-  printBooks(filter(books, filterSentences, FilterType.AND));
+      List<Book> filteredBooks = filter(
+          booksLst: books,
+          filterSentences: [
+            'en 2024',
+            'tendance crypto',
+          ],
+          filterType: FilterType.AND,
+          ignoreCase: false);
 
-  print(
-      '\nbooks list after OR filter sentences <en 2024> and <tendance crypto>');
-  printBooks(filter(books, filterSentences, FilterType.OR));
+      expect(filteredBooks, expectedFilteredBooks);
+    });
+    test('filter by <en 2024> OR <tendance crypto>', () {
+      List<Book> expectedFilteredBooks = [
+        bookOne,
+        bookTwo,
+        bookThree,
+      ];
 
-  filterSentences = [
-    'quelle',
-    '2024',
-  ];
+      List<Book> filteredBooks = filter(
+          booksLst: books,
+          filterSentences: [
+            'en 2024',
+            'tendance crypto',
+          ],
+          filterType: FilterType.OR,
+          ignoreCase: false);
 
-  print('\nbooks list after AND filter sentences <quelle> and <2024>');
-  printBooks(filter(books, filterSentences, FilterType.AND));
+      expect(filteredBooks, expectedFilteredBooks);
+    });
+    test('filter by <quelle> AND <2024>', () {
+      List<Book> expectedFilteredBooks = [
+        bookOne,
+        bookThree,
+      ];
 
-  print('\nbooks list after OR filter sentences <quelle> and <2024>');
-  printBooks(filter(books, filterSentences, FilterType.OR));
+      List<Book> filteredBooks = filter(
+          booksLst: books,
+          filterSentences: [
+            'quelle',
+            '2024',
+          ],
+          filterType: FilterType.AND,
+          ignoreCase: false);
 
-  filterSentences = [
-    '2024',
-    'quelle',
-  ];
+      expect(filteredBooks, expectedFilteredBooks);
+    });
+    test('filter by <quelle> OR <2024>', () {
+      List<Book> expectedFilteredBooks = [
+        bookOne,
+        bookTwo,
+        bookThree,
+        bookFour,
+      ];
 
-  print('\nbooks list after AND filter sentences <2024> and <quelle>');
-  printBooks(filter(books, filterSentences, FilterType.AND));
+      List<Book> filteredBooks = filter(
+          booksLst: books,
+          filterSentences: [
+            'quelle',
+            '2024',
+          ],
+          filterType: FilterType.OR,
+          ignoreCase: false);
 
-  print('\nbooks list after OR filter sentences <2024> and <quelle>');
-  printBooks(filter(books, filterSentences, FilterType.OR));
+      expect(filteredBooks, expectedFilteredBooks);
+    });
+    test('filter by <2024> AND <quelle>', () {
+      List<Book> expectedFilteredBooks = [
+        bookOne,
+        bookThree,
+      ];
+
+      List<Book> filteredBooks = filter(
+          booksLst: books,
+          filterSentences: [
+            '2024',
+            'quelle',
+          ],
+          filterType: FilterType.AND,
+          ignoreCase: false);
+
+      expect(filteredBooks, expectedFilteredBooks);
+    });
+    test('filter by <intelligence> OR <artificielle>', () {
+      List<Book> expectedFilteredBooks = [
+        bookFour,
+      ];
+
+      List<Book> filteredBooks = filter(
+          booksLst: books,
+          filterSentences: [
+            'intelligence',
+            'artificielle',
+          ],
+          filterType: FilterType.OR,
+          ignoreCase: false);
+
+      expect(filteredBooks, expectedFilteredBooks);
+    });
+    test('filter by <2024> OR <quelle>', () {
+      List<Book> expectedFilteredBooks = [
+        bookOne,
+        bookTwo,
+        bookThree,
+        bookFour,
+      ];
+
+      List<Book> filteredBooks = filter(
+          booksLst: books,
+          filterSentences: [
+            '2024',
+            'quelle',
+          ],
+          filterType: FilterType.OR,
+          ignoreCase: false);
+
+      expect(filteredBooks, expectedFilteredBooks);
+    });
+  });
 }
 
-void printBooks(List<Book> books) {
-  for (Book book in books) {
-    print('title: ${book.title}');
-  }
-}
-
-List<Book> filter(
-  List<Book> booksLst,
-  List<String> filterSentences,
-  FilterType filterType,
-) {
+List<Book> filter({
+  required List<Book> booksLst,
+  required List<String> filterSentences,
+  required FilterType filterType,
+  required bool ignoreCase,
+}) {
   List<Book> filteredBooks = [];
   for (Book book in booksLst) {
     bool isBookFiltered = false;
     for (String filterSentence in filterSentences) {
-      if (book.title.toLowerCase().contains(filterSentence.toLowerCase())) {
+      if (ignoreCase
+          ? book.title.toLowerCase().contains(filterSentence.toLowerCase())
+          : book.title.contains(filterSentence)) {
         isBookFiltered = true;
         if (filterType == FilterType.OR) {
           break;
