4d8adc7dfbd6e0605f46595241d7c1d957d58a09
Archanciel <jp.schnyder@gmail.com>
2024-04-23 22:00:37 +0200

    corrected too large dialog content

--- lib/views/widgets/set_audio_speed_dialog_widget.dart
+++ lib/views/widgets/set_audio_speed_dialog_widget.dart
@@ -211,7 +211,8 @@ class _SetAudioSpeedDialogWidgetState extends State<SetAudioSpeedDialogWidget>
     AudioPlayerVM audioGlobalPlayerVM,
     ThemeProviderVM themeProviderVM,
   ) {
-    final speeds = [0.7, 1.0, 1.25, 1.5, 2.0];
+    final speeds = [0.7, 1.0, 1.25, 1.5]; // [0.7, 1.0, 1.25, 1.5, 2.0] is too
+    //                                       large for the screen on S20
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: speeds.map((speed) {
