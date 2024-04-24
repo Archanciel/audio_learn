32b844fb7cbee6d8d742bf387ac85d3d2e88247f
Archanciel <jp.schnyder@gmail.com>
2024-04-25 01:39:54 +0200

    enfin all tsts ok

--- test/util/dir_util_test.dart
+++ test/util/dir_util_test.dart
@@ -12,37 +12,37 @@ void main() {
     test(
       'replacing "/storage/emulated/0/Download/audiolear" by "C:\\Users\\Jean-Pierre\\Development\\Flutter\\audio_learn\\test\\data\\audio"',
       () {
-      // Purge the test playlist directory if it exists so that the
-      // playlist list is empty
-      DirUtil.deleteFilesInDirAndSubDirs(
-        rootPath: kDownloadAppTestDirWindows,
-        deleteSubDirectoriesAsWell: true,
-      );
+        // Purge the test playlist directory if it exists so that the
+        // playlist list is empty
+        DirUtil.deleteFilesInDirAndSubDirs(
+          rootPath: kDownloadAppTestDirWindows,
+          deleteSubDirectoriesAsWell: true,
+        );
 
-      // Copy the test initial audio data to the app dir
-      DirUtil.copyFilesFromDirAndSubDirsToDirectory(
-        sourceRootPath:
-            "$kDownloadAppTestSavedDataDir${path.separator}dir_util_test",
-        destinationRootPath: kDownloadAppTestDirWindows,
-      );
+        // Copy the test initial audio data to the app dir
+        DirUtil.copyFilesFromDirAndSubDirsToDirectory(
+          sourceRootPath:
+              "$kDownloadAppTestSavedDataDir${path.separator}dir_util_test",
+          destinationRootPath: kDownloadAppTestDirWindows,
+        );
 
         DirUtil.replacePlaylistRootPathInSettingsJsonFiles(
             "C:\\Users\\Jean-Pierre\\Development\\Flutter\\audio_learn\\test\\data\\audio",
             '/storage/emulated/0/Download/audiolear',
             "C:\\Users\\Jean-Pierre\\Development\\Flutter\\audio_learn\\test\\data\\audio");
 
-      File expectedFile = File(
-          "$kDownloadAppTestDirWindows${path.separator}test_result.json");
-      File actualFile = File(
-          "$kDownloadAppTestDirWindows${path.separator}settings.json");
-      String actual = actualFile.readAsStringSync();
-      expect(actual, expectedFile.readAsStringSync());
-      
-      // Cleanup the test data directory
-      DirUtil.deleteFilesInDirAndSubDirs(
-        rootPath: kDownloadAppTestDirWindows,
-        deleteSubDirectoriesAsWell: true,
-      );
+        File expectedFile = File(
+            "$kDownloadAppTestDirWindows${path.separator}test_result.json");
+        File actualFile =
+            File("$kDownloadAppTestDirWindows${path.separator}settings.json");
+        String actual = actualFile.readAsStringSync();
+        expect(actual, expectedFile.readAsStringSync());
+
+        // Cleanup the test data directory
+        DirUtil.deleteFilesInDirAndSubDirs(
+          rootPath: kDownloadAppTestDirWindows,
+          deleteSubDirectoriesAsWell: true,
+        );
       },
     );
   });
