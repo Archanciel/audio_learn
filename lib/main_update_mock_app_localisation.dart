9e7b06d317fbf4dbb344113cac0e929000e7a237
Archanciel <jp.schnyder@gmail.com>
2024-05-06 15:14:14 +0200

    renamed file

--- lib/main_update_mock_app_localisation.dart
+++ lib/main_update_mock_app_localisation.dart
@@ -1,21 +1,15 @@
 import 'dart:convert';
 
 /// Executing in vs code Terminal:
-/// 
+///
 /// dart run ./lib/main_create_mock_app_localisation.dart
 void main() {
-  // Simulated content of an ARB file as a JSON string, including a parameterized string
+  // Simulated content of an ARB file as a JSON string,
+  // including a parameterized string
   var arbContent = '''
   {
-    "defaultApplicationHelpTitle": "Default Application",
-    "defaultApplicationHelpContent": "If no option is selected, the defined playback speed will only apply to newly created playlists.",
-    "modifyingExistingPlaylistsHelpTitle": "Modifying Existing Playlists",
-    "modifyingExistingPlaylistsHelpContent": "By selecting the first checkbox, all existing playlists will be set to use the new playback speed. However, this change will only affect audio files that are downloaded after this option is enabled.",
-    "alreadyDownloadedAudiosHelpTitle": "Already Downloaded Audios",
-    "alreadyDownloadedAudiosHelpContent": "Selecting the second checkbox allows you to change the playback speed for audio files already present on the device.",
-    "excludingFutureDownloadsHelpTitle": "Excluding Future Downloads",
-    "excludingFutureDownloadsHelpContent": "If only the second checkbox is checked, the playback speed will not be modified for audios that will be downloaded later in existing playlists. However, as mentioned previously, new playlists will use the newly defined playback speed for all downloaded audios.",
-    "audioNotCopiedFromYoutubePlaylistToLocalPlaylist": "Audio \\"{audioTitle}\\" NOT copied from Youtube playlist \\"{fromPlaylistTitle}\\" to local playlist \\"{toPlaylistTitle}\\" since it is already present in the destination playlist."
+  "alreadyDownloadedAudiosPlaylistHelpTitle": "Already Downloaded Audios",
+  "alreadyDownloadedAudiosPlaylistHelpContent": "Selecting the checkbox allows you to change the playback speed for playlist audio files already present on the device."
   }
   ''';
 
