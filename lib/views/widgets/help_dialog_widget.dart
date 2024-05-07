f3b36ebd9044f6262d17a1a026ea04eb46a69804
Archanciel <jp.schnyder@gmail.com>
2024-05-07 21:48:12 +0200

    renamed

--- lib/views/widgets/help_dialog_widget.dart
+++ lib/views/widgets/help_dialog_widget.dart
@@ -9,11 +9,11 @@ import '../../services/settings_data_service.dart';
 import '../../viewmodels/theme_provider_vm.dart';
 import '../screen_mixin.dart';
 
-class HelpDialog extends StatelessWidget with ScreenMixin {
+class HelpDialogWidget extends StatelessWidget with ScreenMixin {
   final List<HelpItem> helpItemsLst;
   final FocusNode dialogFocusNode = FocusNode();
 
-  HelpDialog({
+  HelpDialogWidget({
     super.key,
     required this.helpItemsLst,
   });
